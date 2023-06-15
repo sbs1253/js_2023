c30cc8d76de9fe53a732b60649a42a639961b438
songbbang <rokaf340@gmail.com>
2023-06-15 16:14:14 +0900

    study

--- 7.parallax-1/js/index.js
+++ 7.parallax-1/js/index.js
@@ -1,7 +1,11 @@
 const imageAll = document.querySelectorAll('.imageWrap .parallaxImage');
+const subPage = document.querySelector('.subPage .parallaxImage');
 
 let scrollNum = 0;
 let image = 0;
+let move = 0;
+const speed = 0.2;
+let targatX = 0;
 window.addEventListener('scroll', () => {
   scrollNum = window.scrollY;
 
@@ -10,3 +14,20 @@ window.addEventListener('scroll', () => {
     image.style.transform = `translateY(${-scrollNum * 0.1 * index}px)`;
   });
 });
+
+window.addEventListener('mousemove', (e) => {
+  move = e.pageX - window.innerWidth / 2;
+});
+
+const loop = () => {
+  targatX += (move - targatX) * speed;
+  imageAll[4].style.transform = `scale(1.1) translate(${-targatX / 200}px, ${
+    -scrollNum * 0.1 * 4
+  }px)`;
+  imageAll[5].style.transform = `scale(1.1) translate(${-targatX / 100}px, ${
+    -scrollNum * 0.1 * 5
+  }px)`;
+  subPage.style.transform = `scale(1.1) translateX(${-targatX / 20}px)`;
+  requestAnimationFrame(loop);
+};
+loop();
